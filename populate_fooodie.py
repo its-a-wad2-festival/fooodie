c2e60354d92bc8e6d53b40fdd63d0483606f4eec
Santi Tomas <2406295t@student.gla.ac.uk>
2020-03-04 16:17:44 +0100

    Added code in populate_fooodie to create user and its /media/<username>/ folder

--- populate_fooodie.py
+++ populate_fooodie.py
@@ -4,12 +4,25 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 
 import django
 django.setup()
-import factory  
-import factory.django
-import django.contrib.auth
-from foodie.models import Photo, UserProfile
+from django.contrib.auth.models import User
+from fooodie.models import Photo, UserProfile
 import random
+import os
+
+#Variable declaration
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MEDIA_DIR = os.path.join(os.path.join(BASE_DIR, 'media'))
+
 
 #Might be useful to create random data for users: stackoverflow.com/questions/33024510/populate-django-database
 
-
+def create_user_profile():
+    user = User.objects.create_user("a","a@gmail.com","12gol34demessi")
+    profile = UserProfile(user=user)
+    profile.save()
+    folder_path = os.path.join(MEDIA_DIR, user.username)
+    os.mkdir(folder_path)
+     
+if __name__ == '__main__':
+    print('Starting fooodie population script...')
+    create_user_profile()
