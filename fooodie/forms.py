d636923612b880fd2580243747dd401db65e838c
Declan Davidson <2038796D@student.gla.ac.uk>
2020-03-07 22:22:23 +0000

    Implementation of profile creation form

---
+++ fooodie/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from fooodie.models import UserProfile, Photo
+from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
