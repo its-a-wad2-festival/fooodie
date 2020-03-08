7544cb50a2b92d0642479a6095b3a0dd043f9117
“Jarvis <2431184t@student.gla.ac.uk>
2020-03-08 21:11:46 +0000

    home page

--- fooodie/urls.py
+++ fooodie/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('leaderboard/', views.leaderboard, name = 'leaderboard'),
-    path('register/', views.register, name = 'register'),]
+    path('register/', views.register, name = 'register'),
+    ]
 """path('add_photo/', views.add_photo, name='add_photo'),
 path('user/<slug:user_name_slug>/', views.restricted, name='user'),
 path('logout/', views.user_logout, name='logout'),"""
-
